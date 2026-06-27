ede73e6e926aa4006e3187c37744ad6a91ac6f82
ARROZ <ernestodiascecilio@gmail.com>
2026-06-27 17:23:11 -0300

    Finalizado o Carrossel e mais melhoras

--- gscr/main.js
+++ gscr/main.js
@@ -2,20 +2,20 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
+  
   pagination: {
     el: '.swiper-pagination',
+    type: 'bullets',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false, 
+    pauseOnMouseEnter: true,
   },
 });
